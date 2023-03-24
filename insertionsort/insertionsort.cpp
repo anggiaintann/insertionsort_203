36b5492c8d2100d08d347bc9a47a6db76292f9a1
anggiaintan <anggia.intan.ft22@mail.umy.ac.id>
2023-03-24 20:31:16 +0700

    membuat looping while

--- insertionsort/insertionsort.cpp
+++ insertionsort/insertionsort.cpp
@@ -43,3 +43,14 @@ void insertionsort() {		// Procedure insertionsort
 
 		j = i - 1;					// 3. Setting nilai j sama dengan i-1;
 
+		while (j >= 0 && arr[j] > temp) // 4. Looping while dimana j lebih besar sma dengan 0 dan
+			// arr[j] lebih besar daripada temp
+		{
+			arr[j + 1] = arr[j];		// 4a. Simpan arr[j] ke dalam variable arr[j+1]
+			j--;						// 4b. Decrement nilai j by 1
+		}
+
+		arr[j + 1] = temp;				// 5. Simpan nilai temp ke dalam arr[j+1]
+
+
+
