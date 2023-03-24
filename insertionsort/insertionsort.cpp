eedb9c8345868a7294c3faccc783c898f0d48d1d
anggiaintan <anggia.intan.ft22@mail.umy.ac.id>
2023-03-24 20:10:32 +0700

    membuat while loop

--- insertionsort/insertionsort.cpp
+++ insertionsort/insertionsort.cpp
@@ -5,3 +5,16 @@ int arr[20];				// Membuat Array dengan panjang data 20
 int n;						// Membuat Variable inputan n
 
 void input() {		// Procedure input
+	while (true)
+	{
+		cout << "Masukkan jumlah data pada array : "; // Membuat inputan jumlah element array
+		cin >> n;									// Memanggil variable inputan n
+
+		if (n <= 20) {								// Membuat kondisi n tidak lebih dari 20
+			break;
+		}
+		else
+		{
+			cout << "\nArray yang anda masukkan maksimal 20 elemen.\n"; // Menampilkan pesan jika data lebih dari 20
+		}
+	}
